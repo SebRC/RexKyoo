f9f2d2d80aed16befb62e0da25b42811db97f6b8
Sebastian Christiansen <seba4746@stud.kea.dk>
2019-11-25 12:36:13 +0100

    Create project.

---
+++ src/main/java/project/rexkyoo/RexkyooApplication.java
@@ -0,0 +1,15 @@
+package project.rexkyoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RexkyooApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RexkyooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/rexkyoo/RexkyooApplicationTests.java
@@ -0,0 +1,15 @@
+package project.rexkyoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RexkyooApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
