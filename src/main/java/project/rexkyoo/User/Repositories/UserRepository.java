dca6c6c62719dbc75ac286ee56a9020328d8a617
mathias@grotrian.dk <math524s@stud.kea.dk>
2019-11-26 14:26:21 +0100

    Create user repository

---
+++ src/main/java/project/rexkyoo/User/Repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package project.rexkyoo.User.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import project.rexkyoo.User.Models.UserModel;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Integer>
+{
+}
