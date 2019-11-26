54caeac6e9f1054badaf7823bad5dfa043c668df
Sebastian Christiansen <seba4746@stud.kea.dk>
2019-11-26 10:22:35 +0100

    Create Customer class with fields

---
+++ src/main/java/project/rexkyoo/Customer/Models/Customer.java
@@ -0,0 +1,16 @@
+package project.rexkyoo.Customer.Models;
+
+import java.util.List;
+
+public class Customer
+{
+    private String name;
+    private String phone;
+    private String address;
+    private String email;
+    private List<Double> payments;
+    private List<Double> expenses;
+    private List<String> ambassadors;
+    private String zipCode;
+    private String city;
+}
