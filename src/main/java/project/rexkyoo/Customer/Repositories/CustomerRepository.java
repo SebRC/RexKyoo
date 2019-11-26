9198e52f1ddf98cd600632b054c962bed129e424
Sebastian Christiansen <seba4746@stud.kea.dk>
2019-11-26 10:38:38 +0100

    Add @Repository annotation to CustomerRepository

--- src/main/java/project/rexkyoo/Customer/Repositories/CustomerRepository.java
+++ src/main/java/project/rexkyoo/Customer/Repositories/CustomerRepository.java
@@ -1,8 +1,10 @@
 package project.rexkyoo.Customer.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import project.rexkyoo.Customer.Models.CustomerModel;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<CustomerModel, Integer>
 {
 }
