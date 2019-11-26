7442d05a39dffe030378a54be3af78a8ffe990ff
Sebastian Christiansen <seba4746@stud.kea.dk>
2019-11-26 10:23:36 +0100

    Add constructor for Customer class

--- src/main/java/project/rexkyoo/Customer/Models/Customer.java
+++ src/main/java/project/rexkyoo/Customer/Models/Customer.java
@@ -13,4 +13,17 @@ public class Customer
     private List<String> ambassadors;
     private String zipCode;
     private String city;
+
+    public Customer(String name, String phone, String address, String email, List<Double> payments, List<Double> expenses, List<String> ambassadors, String zipCode, String city)
+    {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.email = email;
+        this.payments = payments;
+        this.expenses = expenses;
+        this.ambassadors = ambassadors;
+        this.zipCode = zipCode;
+        this.city = city;
+    }
 }
