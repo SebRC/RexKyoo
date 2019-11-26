8fa8c4e2af46d99148812517f13502b79bb8e843
mathias@grotrian.dk <math524s@stud.kea.dk>
2019-11-26 14:26:29 +0100

    Create user service

---
+++ src/main/java/project/rexkyoo/User/Services/UserService.java
@@ -0,0 +1,20 @@
+package project.rexkyoo.User.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import project.rexkyoo.User.Models.UserModel;
+import project.rexkyoo.User.Repositories.UserRepository;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+@Service
+public class UserService
+{
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserModel getOne(String id)
+    {
+        throw new NotImplementedException();
+    }
+
+}
