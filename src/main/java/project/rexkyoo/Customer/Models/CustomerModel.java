e531b3d86359432573370cfc331ebffb6e2b16d8
Sebastian Christiansen <seba4746@stud.kea.dk>
2019-11-26 10:50:25 +0100

    Add getters and setters for CustomerModel

--- src/main/java/project/rexkyoo/Customer/Models/CustomerModel.java
+++ src/main/java/project/rexkyoo/Customer/Models/CustomerModel.java
@@ -32,4 +32,99 @@ public class CustomerModel
         this.zipCode = zipCode;
         this.city = city;
     }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public List<Double> getPayments()
+    {
+        return payments;
+    }
+
+    public void setPayments(List<Double> payments)
+    {
+        this.payments = payments;
+    }
+
+    public List<Double> getExpenses()
+    {
+        return expenses;
+    }
+
+    public void setExpenses(List<Double> expenses)
+    {
+        this.expenses = expenses;
+    }
+
+    public List<String> getAmbassadors()
+    {
+        return ambassadors;
+    }
+
+    public void setAmbassadors(List<String> ambassadors)
+    {
+        this.ambassadors = ambassadors;
+    }
+
+    public String getZipCode()
+    {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode)
+    {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
 }
