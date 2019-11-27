9091b33c40a63cf951ac1f188bc2ac6e2cbf75cb
Tinomotjo <Tino0118@stud.kea.dk>
2019-11-27 11:26:15 +0100

    Changeing typo.

--- src/main/java/project/rexkyoo/CleaningInspector/Repositories/CleaningInspectorRepository.java
+++ src/main/java/project/rexkyoo/CleaningInspector/Repositories/CleaningInspectorRepository.java
@@ -2,8 +2,8 @@ package project.rexkyoo.CleaningInspector.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import project.rexkyoo.Customer.Models.CustomerModel;
+import project.rexkyoo.CleaningInspector.Models.CleaningInspectorModel;
 
 @Repository
-public interface CleaningInspectorRepository extends JpaRepository<CustomerModel, Integer> {
+public interface CleaningInspectorRepository extends JpaRepository<CleaningInspectorModel, Integer> {
 }
