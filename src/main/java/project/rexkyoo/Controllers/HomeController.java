57aae0760c8f7ccaedd725dab125ff2f1c22b83e
mathias@grotrian.dk <math524s@stud.kea.dk>
2019-11-28 11:30:12 +0100

    Set up controller

--- src/main/java/project/rexkyoo/Controllers/HomeController.java
+++ src/main/java/project/rexkyoo/Controllers/HomeController.java
@@ -1,11 +1,16 @@
 package project.rexkyoo.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("backend/home")
+@RequestMapping("/dashboard")
 public class HomeController
 {
-
+    @GetMapping("/home")
+    public String home()
+    {
+        return "backend/home";
+    }
 }
