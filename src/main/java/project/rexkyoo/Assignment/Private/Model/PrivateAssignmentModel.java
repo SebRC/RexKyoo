9e6ddeb6bc46b9a78ce035107a69adb6d6625669
Tinomotjo <Tino0118@stud.kea.dk>
2019-11-28 10:26:36 +0100

    Add expenses list to both assignemts.

--- src/main/java/project/rexkyoo/Assignment/Private/Model/PrivateAssignmentModel.java
+++ src/main/java/project/rexkyoo/Assignment/Private/Model/PrivateAssignmentModel.java
@@ -2,6 +2,7 @@ package project.rexkyoo.Assignment.Private.Model;
 
 import project.rexkyoo.Customer.CustomerModel;
 import project.rexkyoo.Customer.Private.Model.PrivateCustomerModel;
+import project.rexkyoo.Expenses.Models.ExpensesModel;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -17,20 +18,22 @@ public class PrivateAssignmentModel {
     private Date startDate;
     private Date endDate;
     private String type;
-    private PrivateCustomerModel privateCustomerModel;
+    private PrivateCustomerModel privateCustomers;
     private String ambassador;
+    private ExpensesModel expenses;
 
     public PrivateAssignmentModel()
     {}
 
-    public PrivateAssignmentModel(int income, Date startDate, Date endDate, String type, PrivateCustomerModel privateCustomerModel, String ambassador)
+    public PrivateAssignmentModel(int income, Date startDate, Date endDate, String type, PrivateCustomerModel privateCustomerModel, String ambassador,ExpensesModel expenses)
     {
         this.income = income;
         this.startDate = startDate;
         this.endDate = endDate;
         this.type = type;
-        this.privateCustomerModel = privateCustomerModel;
+        this.privateCustomers = privateCustomerModel;
         this.ambassador = ambassador;
+        this.expenses = expenses;
     }
 
     public int getId() {
@@ -71,12 +74,12 @@ public class PrivateAssignmentModel {
 
     public PrivateCustomerModel getPrivateCustomerModel()
     {
-        return privateCustomerModel;
+        return privateCustomers;
     }
 
     public void setPrivateCustomerModel(PrivateCustomerModel privateCustomerModel)
     {
-        this.privateCustomerModel = privateCustomerModel;
+        this.privateCustomers = privateCustomerModel;
     }
 
     public String getAmbassador()
@@ -87,5 +90,18 @@ public class PrivateAssignmentModel {
     public void setAmbassador(String ambassador)
     {
         this.ambassador = ambassador;
+    }
+
+    public PrivateCustomerModel getPrivateCustomers() {
+        return privateCustomers;
+    }
+
+
+    public ExpensesModel getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(ExpensesModel expenses) {
+        this.expenses = expenses;
     }
 }
