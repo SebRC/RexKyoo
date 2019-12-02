8eaad599e5ab72fa5c4d7663f3850dab68295536
ultracake <jackievuong@live.dk>
2019-12-02 14:36:18 +0100

    css bug fix
    the problem was the static folder was static.css and folder should be like static/css

--- src/main/java/project/rexkyoo/Controllers/HomeController.java
+++ src/main/java/project/rexkyoo/Controllers/HomeController.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/dashboard")
+@RequestMapping("/")
 public class HomeController
 {
     @GetMapping("/home")
     public String home()
     {
-        return "dashboard/home";
+        return "/home";
     }
+
 }
