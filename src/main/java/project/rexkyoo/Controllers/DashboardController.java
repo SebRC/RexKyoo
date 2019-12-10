f9e3151a626b29512e1fb3c336b4260fc9fd4703
mathias@grotrian.dk <math524s@stud.kea.dk>
2019-12-10 13:04:33 +0100

    Add autowired customer service to DashboardController

--- src/main/java/project/rexkyoo/Controllers/DashboardController.java
+++ src/main/java/project/rexkyoo/Controllers/DashboardController.java
@@ -1,15 +1,24 @@
 package project.rexkyoo.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import project.rexkyoo.Customer.Model.CustomerModel;
+import project.rexkyoo.Customer.Service.CustomerService;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController
 {
+
+    @Autowired
+    private CustomerService customerService;
+
+
     @GetMapping("/home")
     public String home()
     {
