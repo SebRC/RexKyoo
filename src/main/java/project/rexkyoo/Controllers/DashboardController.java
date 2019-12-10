4575cbb2ab426f34ce4c42d66ac8f0942cf42f54
mathias@grotrian.dk <math524s@stud.kea.dk>
2019-12-10 13:05:05 +0100

    Add POST mapping for creating a customer

--- src/main/java/project/rexkyoo/Controllers/DashboardController.java
+++ src/main/java/project/rexkyoo/Controllers/DashboardController.java
@@ -45,6 +45,15 @@ public class DashboardController
         return "dashboard/create_customer";
     }
 
+    @PostMapping("/customer")
+    public String createCustomer(@ModelAttribute CustomerModel customerModel)
+    {
+        customerService.save(customerModel);
+
+        //  should later redirect to created customer
+        return "redirect:/dashboard/home";
+    }
+
     @GetMapping("privateCustomer")
     public String privateCustomerOverview() { return "dashboard/private_customer_overview";}
 
