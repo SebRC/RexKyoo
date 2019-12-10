6db486466c19e9e9817e760ba6dd1375aa9d4db4
mathias@grotrian.dk <math524s@stud.kea.dk>
2019-12-10 10:00:11 +0100

    add getmapping for create customer

--- src/main/java/project/rexkyoo/Controllers/DashboardController.java
+++ src/main/java/project/rexkyoo/Controllers/DashboardController.java
@@ -1,8 +1,10 @@
 package project.rexkyoo.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import project.rexkyoo.Customer.Model.CustomerModel;
 
 @Controller
 @RequestMapping("/dashboard")
@@ -24,5 +26,13 @@ public class DashboardController
     public String businessCustomerDetails()
     {
         return "dashboard/business_customer_details";
+    }
+
+    @GetMapping("/customer")
+    public String createCustomer(Model model)
+    {
+        model.addAttribute("customerModel", new CustomerModel());
+
+        return "dashboard/create_customer";
     }
 }
