ed6993b644c11cf6c923c562fff9c0da7bbb6434
Sebastian Christiansen <seba4746@stud.kea.dk>
2019-12-17 16:16:09 +0100

    Delete related payment records when customer is deleted

--- src/main/java/project/rexkyoo/Customer/Model/CustomerModel.java
+++ src/main/java/project/rexkyoo/Customer/Model/CustomerModel.java
@@ -41,7 +41,7 @@ public class CustomerModel
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer")
     private Set<FeedbackModel> feedbacks;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer")
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer" , cascade=CascadeType.ALL, orphanRemoval=true)
     private Set<CustomerPaymentDateModel> customerPaymentDates;
 
     public CustomerModel()
