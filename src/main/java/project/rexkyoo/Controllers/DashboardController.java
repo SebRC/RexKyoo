b0845346aa68935b0eb86617cb6a4320aeb7cfe2
Tinomotjo <Tino0118@stud.kea.dk>
2019-12-18 13:34:39 +0100

    creating economy controller

--- src/main/java/project/rexkyoo/Controllers/DashboardController.java
+++ src/main/java/project/rexkyoo/Controllers/DashboardController.java
@@ -3,22 +3,60 @@ package project.rexkyoo.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import project.rexkyoo.Ambassador.Models.AmbassadorModel;
+import project.rexkyoo.Ambassador.Services.AmbassadorService;
+import project.rexkyoo.Assignment.Model.AssignmentModel;
+import project.rexkyoo.Assignment.Service.AssignmentService;
+import project.rexkyoo.CleaningInspector.Models.CleaningInspectorModel;
+import project.rexkyoo.CleaningInspector.Services.CleaningInspectorService;
 import project.rexkyoo.Customer.Model.CustomerModel;
 import project.rexkyoo.Customer.Service.CustomerService;
 import project.rexkyoo.CustomerPaymentDate.Model.CustomerPaymentDateModel;
 import project.rexkyoo.CustomerPaymentDate.Service.CustomerPaymentDateService;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/admin")
 public class DashboardController
 {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private AssignmentService assignmentService;
+
+    @Autowired
+    private AmbassadorService ambassadorService;
+
+    @Autowired
+    private CleaningInspectorService cleaningInspectorService;
+
+
+
+
     @GetMapping("/home")
     public String home()
     {
         return "dashboard/home";
+    }
+
+    @GetMapping("/economy")
+    public String economy(Model model)
+    {
+        List<CustomerModel> customers = customerService.getAll();
+        List<AmbassadorModel> ambassadors = ambassadorService.getAll();
+        List<CleaningInspectorModel> cleaningInspectors = cleaningInspectorService.getAll();
+        List<CustomerModel> assignments = customerService.getAll();
+
+        model.addAttribute("customers", customers);
+        model.addAttribute("ambassadors", ambassadors);
+        model.addAttribute("cleaningInspectors", cleaningInspectors);
+        model.addAttribute("assignments", assignments);
+
+
+        return "dashboard/economy";
     }
 }
