9d0eebe1fa55992ee35c8a47645f469b91a9bd43
Sebastian Christiansen <seba4746@stud.kea.dk>
2019-12-19 13:22:11 +0100

    Add successful test for getSpecificYearPaymentDates

---
+++ src/test/java/project/rexkyoo/CustomerPaymentDateServiceTests/GetSpecificYearPaymentDates.java
@@ -0,0 +1,91 @@
+package project.rexkyoo.CustomerPaymentDateServiceTests;
+
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.*;
+import project.rexkyoo.Customer.Model.CustomerModel;
+import project.rexkyoo.CustomerPaymentDate.Model.CustomerPaymentDateModel;
+import project.rexkyoo.CustomerPaymentDate.Service.CustomerPaymentDateService;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@RunWith(Enclosed.class)
+public class GetSpecificYearPaymentDates
+{
+    @RunWith(Parameterized.class)
+    public static class ValidPaymentDates
+    {
+        private HashSet<CustomerPaymentDateModel> customerPaymentDates;
+        private String expectedResult;
+        private int numberOfSameYears;
+
+        public ValidPaymentDates(HashSet<CustomerPaymentDateModel> customerPaymentDates, String expectedResult, int numberOfSameYears)
+        {
+            this.customerPaymentDates = customerPaymentDates;
+            this.expectedResult = expectedResult;
+            this.numberOfSameYears = numberOfSameYears;
+        }
+
+        private static HashSet<CustomerPaymentDateModel> getValidPaymentDates(int numberOfSameYears)
+        {
+            HashSet<CustomerPaymentDateModel> customerPaymentDates = new HashSet<>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < numberOfSameYears; j++)
+                {
+                    CustomerPaymentDateModel customerPaymentDate =
+                            new CustomerPaymentDateModel("Afventer betaling",
+                                    "201" + i + "-01-01", new CustomerModel());
+
+                    customerPaymentDates.add(customerPaymentDate);
+                }
+            }
+
+            return customerPaymentDates;
+        }
+
+        @Parameterized.Parameters(name= "{index}: Year should be: {1}. Size should be: {2}")
+        public static Iterable<Object[]> validPaymentDatesData()
+        {
+            return Arrays.asList(new Object[][]
+                    {
+                            {getValidPaymentDates(5), "2010", 5},
+                            {getValidPaymentDates(11), "2011", 11},
+                            {getValidPaymentDates(7), "2012", 7},
+                            {getValidPaymentDates(4), "2013", 4},
+                            {getValidPaymentDates(1), "2014", 1}
+                    }
+            );
+        }
+
+        @Test
+        public void validDates_Succeeds()
+        {
+            // Arrange
+            CustomerPaymentDateService customerPaymentDateService = new CustomerPaymentDateService();
+            Set<CustomerPaymentDateModel> specificYearPaymentDates;
+
+            // Act
+            specificYearPaymentDates = customerPaymentDateService.getSpecificYearPaymentDates(customerPaymentDates, expectedResult);
+
+            for (CustomerPaymentDateModel paymentDate : customerPaymentDates)
+            {
+                customerPaymentDateService.setYear(paymentDate);
+            }
+
+            // Assert
+            for (CustomerPaymentDateModel paymentDate : specificYearPaymentDates)
+            {
+                assertEquals(paymentDate.getYear(), expectedResult);
+            }
+
+            assertEquals(specificYearPaymentDates.size(), numberOfSameYears);
+        }
+
+    }
+}
