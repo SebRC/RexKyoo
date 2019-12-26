b7f9e6ad0958b71161be60b7bdbd84053de81f90
Sebastian Christiansen <seba4746@stud.kea.dk>
2019-12-27 00:50:02 +0100

    Add Repository method for getting all AssignmentModels based on the connected CustomerModels type

--- src/main/java/project/rexkyoo/Assignment/Repository/AssignmentRepository.java
+++ src/main/java/project/rexkyoo/Assignment/Repository/AssignmentRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.rexkyoo.Assignment.Model.AssignmentModel;
 
+import java.util.List;
+
 
 @Repository
 public interface AssignmentRepository extends JpaRepository<AssignmentModel, Integer>
-{}
+{
+    List<AssignmentModel> findAllByCustomerTypeEquals(String type);
+}
