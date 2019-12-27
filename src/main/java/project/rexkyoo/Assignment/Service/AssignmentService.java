65d22ce61f70b4f8c29ef190fc59e0204a69befa
Sebastian Christiansen <seba4746@stud.kea.dk>
2019-12-27 01:10:22 +0100

    Add methods for assigning income and expenses to EconomyModel

--- src/main/java/project/rexkyoo/Assignment/Service/AssignmentService.java
+++ src/main/java/project/rexkyoo/Assignment/Service/AssignmentService.java
@@ -3,11 +3,14 @@ package project.rexkyoo.Assignment.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import project.rexkyoo.Assignment.AssignmentType;
+import project.rexkyoo.Assignment.Model.EconomyModel;
 import project.rexkyoo.Assignment.Repository.AssignmentRepository;
 import project.rexkyoo.Assignment.Model.AssignmentModel;
+import project.rexkyoo.Expenses.Models.ExpenseModel;
 
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class AssignmentService
@@ -42,11 +45,26 @@ public class AssignmentService
         return assignmentTypes;
     }
 
-    public double getIncomeBasedOnCustomerType(String type)
+    public EconomyModel getEconomyBasedOnCustomerType(String type)
     {
-        double privateCustomersIncome = 0;
+        double customersIncome;
+        double customersExpenses;
+        double customersProfit;
 
         List<AssignmentModel> assignments = assignmentRepository.findAllByCustomerTypeEquals(type);
+
+        customersIncome = assignIncome(assignments);
+        customersExpenses = assignExpenses(assignments);
+        customersProfit = customersIncome - customersExpenses;
+
+        EconomyModel economy = new EconomyModel(customersIncome, customersExpenses, customersProfit);
+
+        return economy;
+    }
+
+    private double assignIncome(List<AssignmentModel> assignments)
+    {
+        double privateCustomersIncome = 0;
 
         for (AssignmentModel assignment : assignments)
         {
@@ -54,5 +72,22 @@ public class AssignmentService
         }
 
         return privateCustomersIncome;
+    }
+
+    private double assignExpenses(List<AssignmentModel> assignments)
+    {
+        double privateCustomersExpenses = 0;
+
+        for (AssignmentModel assignment : assignments)
+        {
+            Set<ExpenseModel> expenses = assignment.getExpenses();
+
+            for (ExpenseModel expense : expenses)
+            {
+                privateCustomersExpenses += expense.getPrice();
+            }
+        }
+
+        return privateCustomersExpenses;
     }
 }
