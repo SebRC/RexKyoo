1576b6e9e4d805d27edfb1bae01512e337379fd1
Sebastian Christiansen <seb-chris@live.dk>
2019-12-28 11:57:34 +0100

    Show area chart data on homepage

--- src/main/java/project/rexkyoo/Controllers/HomeController.java
+++ src/main/java/project/rexkyoo/Controllers/HomeController.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import project.rexkyoo.Contract.ContractModel;
+import project.rexkyoo.Contract.ContractService;
 import project.rexkyoo.Economy.EconomyModel;
 import project.rexkyoo.Customer.CustomerService;
 import project.rexkyoo.Economy.EconomyService;
+import project.rexkyoo.Economy.MonthsIncomeModel;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -18,6 +23,9 @@ public class HomeController
     @Autowired
     private EconomyService economyService;
 
+    @Autowired
+    private ContractService contractService;
+
     @GetMapping("/home")
     public String home(Model model)
     {
@@ -27,11 +35,19 @@ public class HomeController
         EconomyModel privateCustomersEconomy = economyService.getEconomyBasedOnCustomerType("private");
         EconomyModel businessCustomersEconomy = economyService.getEconomyBasedOnCustomerType("business");
 
+        List<ContractModel> contracts = contractService.getAll();
+
+        economyService.assignMonthsToContracts(contracts);
+
+        MonthsIncomeModel monthsIncome = economyService.getMonthPayments(contractService.getAll());
+
         model.addAttribute("amountOfPrivateCustomers", amountOfPrivateCustomers);
         model.addAttribute("amountOfBusinessCustomers", amountOfBusinessCustomers);
 
         model.addAttribute("privateCustomersEconomy", privateCustomersEconomy);
         model.addAttribute("businessCustomersEconomy", businessCustomersEconomy);
+
+        model.addAttribute("monthsIncome", monthsIncome);
 
         return "dashboard/home";
     }
