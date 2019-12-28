231f2841a102ac6597fae956674824164a2095f6
Mathias Grotrian <mathias@grotrian.dk>
2019-12-28 10:55:09 +0100

    add password encoder

---
+++ src/main/java/project/rexkyoo/SecurityConfig/WebMVCConfig.java
@@ -0,0 +1,21 @@
+package project.rexkyoo.SecurityConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//configuration class for password encoder
+@Configuration
+public class WebMVCConfig implements WebMvcConfigurer
+{
+
+    //responsible for hashing passwords
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder()
+    {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+        return bCryptPasswordEncoder;
+    }
+}
