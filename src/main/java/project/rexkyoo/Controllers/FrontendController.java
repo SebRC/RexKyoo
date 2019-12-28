d24e072a777d1bf50e80599001aa526555d1c667
Mathias Grotrian <mathias@grotrian.dk>
2019-12-28 10:56:59 +0100

    change frontend controller mapping

--- src/main/java/project/rexkyoo/Controllers/FrontendController.java
+++ src/main/java/project/rexkyoo/Controllers/FrontendController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/dk")
 public class FrontendController
 {
-    @GetMapping("/")
+    @GetMapping("/forside")
     public String forside()
     {
         return "frontend/index";
